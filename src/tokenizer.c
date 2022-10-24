9549f3cf5de1b598b42fc5357d5414174be49e8d
PabloDMartinez <pdmartinez3@miners.utep.edu>
2022-10-24 13:28:44 -0600

    added some functions

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,28 +1,63 @@
-
-#ifndef _TOKENIZER_
-#define _TOKENIZER_
-
+#include<stdlib.h>
+#include<stdio.h>
+#include"tokenizer.h"
 
 /* Return true (non-zero) if c is a whitespace characer
    ('\t' or ' ').  
    Zero terminators are not printable (therefore false) */
-int space_char(char c);
+int space_char(char c){
+  if(c == " " || c == "\t"){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
 
 /* Return true (non-zero) if c is a non-whitespace 
    character (not tab or space).  
    Zero terminators are not printable (therefore false) */ 
-int non_space_char(char c);
+int non_space_char(char c){
+  if(c == " "|| c == "\t" || '\0'){
+    return 0;
+  }
+  else{
+    return 1;
+  }
+}
 
 /* Returns a pointer to the first character of the next 
    space-separated word in zero-terminated str.  Return a zero pointer if 
    str does not contain any words. */
-char *word_start(char *str); 
+char *word_start(char *str){
+  *str = NULL;
+  int *startPos = NULL;
+  if(space_char(c) == 1)
+    *str++;
+  else if((non_space_char(c))==1){
+    *startPos = *str;
+    return *startPos;
+  }
+} 
 
 /* Returns a pointer terminator char following *word */
-char *word_terminator(char *word);
+char *word_terminator(char *word){
+  *word = *word_start(*str);
+  int *endPos = *startPos;
+  if((non_space_char(c)) == 1){
+    *word++;
+  }
+  else if((space_char(c))==1){
+    *word--;
+    *endPos = *word;
+    return *endPos;
+  }
+}
 
 /* Counts the number of words in the string argument. */
-int count_words(char *str);
+int count_words(char *str){
+  
+}
 
 /* Returns a fresly allocated new zero-terminated string 
    containing <len> chars from <inStr> */
@@ -44,5 +79,3 @@ void print_tokens(char **tokens);
 
 /* Frees all tokens and the vector containing themx. */
 void free_tokens(char **tokens);
-
-#endif
---
+++ src/userInterface.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    char prompt[] = "";
+    printf("Welcome.\n");
+    printf("Enter Prompt\n>");
+    scanf("%s", prompt);
+    printf("%s\n", prompt);
+}
